7aace4adf000f6c7827b8cb3c16f0e4fcb751a3c
justinestrada <justin@mydigitalsauce.com>
2019-03-31 21:26:08 -0700

    Project Init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+    	<appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes" ></appHeader>
+    	<appNewQuote @quoteAdded="newQuote" ></appNewQuote>
+		<appQuoteGird :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGird> 
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<div class="alert alert-info">
+					Info: Click on a Quote to delete it.
+				</div>
+			</div>
+		</div>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+	import Header from './components/Header.vue';
+	export default {
+		data: function() {
+			return {
+				quotes: [
+					"Test Quote"
+				],
+				maxQuotes: 10
+			}
+		},
+		methods: {
+			newQuote(quote) {
+				if ( this.quotes.length >= this.maxQuotes ) {
+					return alert("Please delete Quotes first.");
+				}
+				this.quotes.push(quote);
+			},
+			deleteQuote(index) {
+				this.quotes.splice(index, 1);
+			}
+		},
+		components: {
+			appQuoteGird: QuoteGrid,
+			appNewQuote: NewQuote,
+			appHeader: Header,
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="row">
+		<form>
+			<div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+				<div class="form-group">
+					<label for="">Quote</label>
+					<textarea required name="" id="" rows="3" class="form-control" v-model="quote"></textarea>
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary" @click.prevent="createNew" >Add Quote</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+	import QuoteGrid from './QuoteGrid.vue';
+	export default {
+		data: function() {
+			return {
+				quote: ""
+			};
+		},
+		methods: {
+			createNew() {
+				this.$emit( 'quoteAdded', this.quote );
+				this.quote = "";
+			}
+		}
+	}
+</script>
